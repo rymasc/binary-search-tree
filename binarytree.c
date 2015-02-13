db8ecf144a1082ccc094cd63551216a54c205635
RSCOTT Linux <ryan.matt.scott@gmail.com>
2015-02-13 18:05:40 -0500

    Few error fixes, still not compiling

--- binarytree.c
+++ binarytree.c
@@ -2,37 +2,36 @@
 #include <stdlib.h>
 
 typedef struct treenode{
-	int val;
+	int data;
 	struct treenode *left;
 	struct treenode *right;
 }treenode;
 
-void Insert(treenode** root, int data);
-	if (*root == NULL){
-		*root = createNewNode;
-	}
-	else if(data <= root->data){
-		root->left = Insert(root->left,data);
-	} else{
-		root->right = Insert(root->right,data);
-	}
-	
-}
-
-
 treenode* createNewNode(int data){
-	treenode* newNode = (*treenode)malloc(sizeof(treenode));
+	treenode* newNode = malloc(sizeof(treenode));
 	newNode->data = data; // or (*newNode).data = data;
-	newNode->left = NULL:
+	newNode->left = NULL;
 	newNode->right = NULL;
 	return newNode;
 }
 
+void Insert(treenode** root, int data){
+	if (*root == NULL){
+		*root = createNewNode(data);
+	}
+	else if(data <= *root->data){
+		*root->left = Insert(*root->left,data);
+	} else{
+		*root->right = Insert(*root->right,data);
+	}
+}
+
+
 int main(){
 	
 	treenode* root = NULL; //creates empty bst
-	Insert(root, 15);
-	insert(root, 20);	
+	Insert(&root, 15);
+	insert(&root, 20);	
 	//int input[13] = {1,1,3,3,4,4,5,5,6,6,8,12};		
 	return 0;
 }

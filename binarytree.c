ee2f5669b5046a5f46696257d51e7e55643a6c68
RSCOTT Linux <ryan.matt.scott@gmail.com>
2015-02-13 17:40:56 -0500

    add left right links

--- binarytree.c
+++ binarytree.c
@@ -7,7 +7,19 @@ typedef struct treenode{
 	struct treenode *right;
 }treenode;
 
-void Insert(treenode* root, int data);
+void Insert(treenode** root, int data);
+	if (*root == NULL){
+		*root = createNewNode;
+	}
+	else if(data <= root->data){
+		root->left = Insert(root->left,data);
+	} else{
+		root->right = Insert(root->right,data);
+	}
+	
+}
+
+
 treenode* createNewNode(int data){
 	treenode* newNode = (*treenode)malloc(sizeof(treenode));
 	newNode->data = data; // or (*newNode).data = data;
@@ -19,7 +31,8 @@ treenode* createNewNode(int data){
 int main(){
 	
 	treenode* root = NULL; //creates empty bst
-	
-	int input[13] = {1,1,3,3,4,4,5,5,6,6,8,12};		
+	Insert(root, 15);
+	insert(root, 20);	
+	//int input[13] = {1,1,3,3,4,4,5,5,6,6,8,12};		
 	return 0;
 }

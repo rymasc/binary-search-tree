04337a46049433ab6ead8717f3cdbaec8f63d605
Ryan Scott <ryan.matt.scott@gmail.com>
2015-02-13 23:15:16 -0500

    Correct compiling binary tree with search feature

--- binarytree.c
+++ binarytree.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define false 0
+#define true 1
+typedef int bool;
 
 typedef struct treenode {
 	int data;
@@ -15,6 +18,28 @@ treenode* createNewNode(int data){
 	return newNode;
 }
 
+
+treenode* insert(treenode* root, int data){
+	if (root == NULL){
+		root = createNewNode(data);
+	}
+	else if(data <= root->data){
+		root->left = insert(root->left, data);
+	} else{
+		root->right = insert(root->right, data);
+	}
+	return root;
+}
+
+bool search(treenode* root, int data){
+	if(root==NULL) return false;
+	if(root->data == data) return true;
+	else if(data <= root->data) return search(root->left, data);
+	else return search(root->right, data);
+}
+
+
+/*
 void insert(treenode** root, int data){
 	if (*root == NULL){
 		*root = createNewNode(data);
@@ -25,12 +50,14 @@ void insert(treenode** root, int data){
 		insert(&(*root)->right,data);
 	}
 }
-
+*/
 
 int main(){
 	treenode* root = NULL; //creates empty bst
-	insert(&root, 15);
-	insert(&root, 20);	
+	root = insert(root, 15);
+	root = insert(root, 20);
+	printf("root.data = %d", root->data);
+	if (search(root, 15)) printf("15 is in binary tree\n");
 	//int input[13] = {1,1,3,3,4,4,5,5,6,6,8,12};		
 	return 0;
 }
